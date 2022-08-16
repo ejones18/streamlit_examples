cf5953b0b385805446821275448331e991c9afd9
Ethan <ejones18@sheffield.ac.uk>
2022-08-16 17:11:52 +0100

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# streamlit_app_example
+# Example Streamlit applications
+
+A few examples from my learning journey with Streamlit.io
+
+* [Single-page visualisation of NYC Uber data](./example_app/example.py)
+* [Multi-page concept of a blog](./blog_app)
+
+## References / resources
+
+* [API reference](https://docs.streamlit.io/library/api-reference)
+* [Streamlit examples](https://streamlit.io/gallery)
---
+++ blog_app/Homepage.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(
+     page_title="Data For Thought: Homepage",
+ )
+
+st.title('> Welcome to Data For Thought')
+st.subheader('> A data driven blog by Ethan Jones')
+
+st.image("https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2500&h=320&q=80")
+
+st.write('Welcome to my blog! This is the new home of all my posts following my move over from Medium.com. I primarily write about the Data & AI stack on Azure as well as more traditional data science using Python and open-source libraries.')
+st.write('I aim to write and upload a new data-focussed post to my site at least once every six weeks - keep your eyes peeled for new releases!')
+
+st.header('> About Me')
+
+st.write("Currently studying towards a BSc in Computer Science, I have industry experience in both data science and cloud computing - specifically Microsoft Azure. I also have substantial experience working with Python and a variety of open-source libraries such as Pandas in various real-world scenarios.")
+st.write('Outside my studies, I am an assistant data analyst at Sheffield Solar - a renewable energy research group based at The University of Sheffield as well as a co-founder of the Yorkshire Azure User Group.')
+
+st.subheader('> Current focus:')
+st.markdown('🔭 &nbsp;*Currently working on*: Leading the Yorkshire Azure User Group, AutoML for Imagery within Azure Machine Learning & data science using open-source Python.')
+st.markdown('🌱 &nbsp;*Currently learning*: E2E Computer Vision workloads & Data Science on Azure.')
+st.markdown('🤔 &nbsp;*Currently looking for*: I am looking for people to collaborate with on data-orientated projects; preferably with an ML focus. Please feel free to get in touch!')
+
+st.markdown('📫 &nbsp;*Get in touch*:')
+st.markdown('[LinkedIn Profile](https://www.linkedin.com/in/ejjones18/)')
+st.markdown('[Credly Badge Profile](https://www.credly.com/users/ethan-jones.bd1ff17d/badges)')
+
+st.header('~ Ethan')
